7246d66768dbb8b18207850b0a9fed87f6e8b7e6
SarahMussa <72816232+SarahMussa@users.noreply.github.com>
2021-02-21 17:50:22 +0000

    Update Sunrise and Sunset

--- src/Date/DaySunrise.js
+++ src/Date/DaySunrise.js
@@ -3,14 +3,25 @@ import React from "react";
 export default function DaySunrise(props){
 
     let hours = props.sunrise.getHours();
+    let minutes = props.sunrise.getMinutes();
+    let hourFormat = "24h";
+    
+    if (hourFormat === "24h") {
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
     if (hours < 10) {
       hours = `0${hours}`;
     }
 
-    let minutes = props.sunrise.getMinutes();
-    if (minutes < 10) {
-      minutes = `0${minutes}`;
-    }
-
     return (<div>{hours}:{minutes}h</div>);
+  } else {
+    return props.sunrise.toLocaleString("en-US", {
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true
+    });
+  }
 }
+
+
--- src/Date/DaySunset.js
+++ src/Date/DaySunset.js
@@ -3,13 +3,23 @@ import React from "react";
 export default function DaySunset(props){
 
     let hours = props.sunset.getHours();
+    let minutes = props.sunset.getMinutes();
+    let hourFormat = "24h";
+    
+    if (hourFormat === "24h") {
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
     if (hours < 10) {
       hours = `0${hours}`;
     }
 
-    let minutes = props.sunset.getMinutes();
-    if (minutes < 10) {
-      minutes = `0${minutes}`;
-    }
     return (<div>{hours}:{minutes}h</div>);
+  } else {
+    return props.sunset.toLocaleString("en-US", {
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true
+    });
+  }
 }
